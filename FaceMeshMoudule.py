d7e321f0a49d2d15e7f7bc7d72a3fef8fee4f505
tianxin <user_tx@163.com>
2022-07-18 12:37:48 +0800

    cap

--- FaceMeshMoudule.py
+++ FaceMeshMoudule.py
@@ -2,17 +2,40 @@ import cv2
 import mediapipe as mp
 import time
 
-# cap = cv2.VideoCapture("Videos/88.mp4")
+class FaceMeshDetector:
+    def __init__(self, staticMode=False, maxFaces=4, minDetectionCon=0.5, minTrackCon=0.5):
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
 
-class FaceMesh:
-    def __init__(self):
+    # def __init__(self):
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceMesh = mp.solutions.face_mesh
-        self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2) # max_num_faces=1 rgb
+        # self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2) # max_num_faces=1 rgb
+        self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode,self.maxFaces, False,
+                                                 self.minDetectionCon ,self.minTrackCon )
         self.drawSpec= self.mpDraw.DrawingSpec((255,0,0),thickness=1,circle_radius=2)
 
 
-
+    def findFaceMesh(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(imgRGB)
+        faces = []
+        if self.results.multi_face_landmarks:
+            for faceLms in self.results.multi_face_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
+                                               self.drawSpec, self.drawSpec)  # FACE_CONNECTIONS
+                face = []
+                for id, lm in enumerate(faceLms.landmark):
+                    # print(lm)
+                    ih, iw, ic = img.shape
+                    x, y = int(lm.x * iw), int(lm.y * ih)
+                    # print(id, x, y)
+                    face.append([x,y])
+                faces.append(face)
+        return img, faces
 
     def drawPoints(self,img,draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -30,13 +53,16 @@ class FaceMesh:
 
 
 def main():
-    cap = cv2.VideoCapture(0)
-    facemesh = FaceMesh()
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("Videos/22.mp4")
+    facemesh = FaceMeshDetector()
     pTime = 0
     while True:
         suceess, img = cap.read()
-        # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        facemesh.drawPoints(img)
+        img, faces = facemesh.findFaceMesh(img)
+        if len(faces) != 0:
+            print(len(faces))
+        # facemesh.drawPoints(img)
         cTime = time.time()
         fps = 1/(cTime-pTime)
         pTime = cTime
